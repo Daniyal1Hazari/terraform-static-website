9a71d758a8c4cb68452c9a3d0c3003d772184757
Daniyal Hazari <daniyalhazari2000@gmail.com>
2026-06-30 02:47:58 +0000

    Rename IAM role and instance profile

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "ec2_role" {
-  name = "ec2-ssm-role"
+  name = "ec2-ssm-role-v2"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -13,12 +13,7 @@ resource "aws_iam_role" "ec2_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "ssm_attach" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
-
 resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "ec2-instance-profile"
+  name = "ec2-instance-profile-v2"
   role = aws_iam_role.ec2_role.name
 }
