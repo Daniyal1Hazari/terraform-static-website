7b4f297d2680bb45e36d8a9359124735b1f888b0
Daniyal Hazari <daniyalhazari2000@gmail.com>
2026-06-30 00:26:25 +0000

    Add website files

---
+++ website/script.js
@@ -0,0 +1,4 @@
+function showMessage() {
+    document.getElementById("message").innerHTML =
+        "✅ Deployment Successful! Your website is running on AWS EC2 using Terraform and GitHub Actions.";
+}
