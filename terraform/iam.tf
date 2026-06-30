d9fe1f8ecd2b4bb5a6112be6cb1beab88de7e5b9
Daniyal Hazari <daniyalhazari2000@gmail.com>
2026-06-30 10:20:07 +0000

    Commit

---
+++ terraform/iam.tf
@@ -0,0 +1,20 @@
+resource "aws_iam_role" "ec2_role" {
+  name = "my-custom-role-devops"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "custom-instance"
+  role = aws_iam_role.ec2_role.name
+}
