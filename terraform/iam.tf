275f874c1fa0b034e60f04d982e7ba60a94b6dc8
Daniyal Hazari <daniyalhazari2000@gmail.com>
2026-06-30 03:35:30 +0000

    Commit

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "ec2_role" {
-  name = "my-role"
+  name = "my-role-1"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -14,6 +14,6 @@ resource "aws_iam_role" "ec2_role" {
 }
 
 resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "my-profile"
+  name = "my-profile-1"
   role = aws_iam_role.ec2_role.name
 }
