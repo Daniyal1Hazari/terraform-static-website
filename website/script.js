7d5172d1328426bd64c32d2feb51ef3b81c15666
Daniyal Hazari <daniyalhazari2000@gmail.com>
2026-06-30 05:04:24 +0000

    Enhance website landing page

---
+++ website/script.js
@@ -0,0 +1,4 @@
+function showMessage() {
+    document.getElementById("message").innerHTML =
+        "✅ Deployment Successful! This website is running on an AWS EC2 instance provisioned using Terraform and deployed through a GitHub Actions CI/CD pipeline.";
+}
