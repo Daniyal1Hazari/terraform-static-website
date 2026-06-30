5ff74132ab905ae9d98aaf3e4793cb3e45ef08b9
Daniyal1Hazari <166779386+Daniyal1Hazari@users.noreply.github.com>
2026-06-30 06:31:38 +0530

    Delete website directory

--- website/script.js
+++
@@ -1,4 +0,0 @@
-function showMessage() {
-    document.getElementById("message").innerHTML =
-        "✅ Deployment Successful! Your website is running on AWS EC2 using Terraform and GitHub Actions.";
-}
